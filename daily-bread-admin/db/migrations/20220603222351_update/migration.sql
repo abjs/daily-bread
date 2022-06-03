d3e3a4f858dbf3cc1064faa762a76c249cc67491
AbinPM <abin.pm@emvigotech.com>
2022-06-04 05:13:11 +0530

    docs: db update

---
+++ daily-bread-admin/db/migrations/20220603222351_update/migration.sql
@@ -0,0 +1,83 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NULL DEFAULT 'INDIVIDUAL',
+    `organizationId` VARCHAR(191) NULL,
+    `organizationName` VARCHAR(191) NULL,
+    `hashedPassword` VARCHAR(191) NULL,
+    `totalMembers` INTEGER NULL,
+    `address` VARCHAR(191) NULL,
+    `city` VARCHAR(191) NULL,
+    `state` VARCHAR(191) NULL DEFAULT 'KERALA',
+    `country` VARCHAR(191) NULL DEFAULT 'INDIA',
+    `pinCode` VARCHAR(191) NULL,
+    `role` VARCHAR(191) NOT NULL DEFAULT 'USER',
+
+    UNIQUE INDEX `User_email_role_key`(`email`, `role`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Session` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `expiresAt` DATETIME(3) NULL,
+    `handle` VARCHAR(191) NOT NULL,
+    `hashedSessionToken` VARCHAR(191) NULL,
+    `antiCSRFToken` VARCHAR(191) NULL,
+    `publicData` VARCHAR(191) NULL,
+    `privateData` VARCHAR(191) NULL,
+    `userId` INTEGER NULL,
+
+    UNIQUE INDEX `Session_handle_key`(`handle`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Token` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `hashedToken` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `expiresAt` DATETIME(3) NOT NULL,
+    `sentTo` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Token_hashedToken_type_key`(`hashedToken`, `type`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Donation` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `foodItem` VARCHAR(191) NOT NULL,
+    `quantity` INTEGER NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+    `city` VARCHAR(191) NOT NULL,
+    `state` VARCHAR(191) NOT NULL DEFAULT 'KERALA',
+    `country` VARCHAR(191) NOT NULL DEFAULT 'INDIA',
+    `pinCode` VARCHAR(191) NOT NULL,
+    `pickupBy` INTEGER NOT NULL,
+    `status` ENUM('INPROGRESS', 'APPROVED', 'REJECTED', 'COMPLETED', 'ONTHEWAY', 'DELIVERED') NOT NULL DEFAULT 'INPROGRESS',
+    `comments` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Session` ADD CONSTRAINT `Session_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Token` ADD CONSTRAINT `Token_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Donation` ADD CONSTRAINT `Donation_id_fkey` FOREIGN KEY (`id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
