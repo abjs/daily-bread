eb1ff23c069a54541dd1cfa9255aafb4cb6123c5
maheshemvigo <mahesh.v@emvigotech.com>
2022-06-03 23:52:27 +0530

    docs:update form

--- daily-bread-recipient/app/auth/components/SignupForm.tsx
+++ daily-bread-recipient/app/auth/components/SignupForm.tsx
@@ -1,43 +1,58 @@
-import { useMutation } from "blitz"
-import { LabeledTextField } from "app/core/components/LabeledTextField"
-import { Form, FORM_ERROR } from "app/core/components/Form"
-import signup from "app/auth/mutations/signup"
-import { Signup } from "app/auth/validations"
+import { useMutation } from 'blitz';
+import { LabeledTextField } from 'app/core/components/LabeledTextField';
+import { Form, FORM_ERROR } from 'app/core/components/Form';
+import signup from 'app/auth/mutations/signup';
+import { Signup } from 'app/auth/validations';
+import LabeledPasswordField from 'app/core/components/LabeledPasswordField';
 
 type SignupFormProps = {
-  onSuccess?: () => void
-}
+	onSuccess?: () => void;
+};
 
 export const SignupForm = (props: SignupFormProps) => {
-  const [signupMutation] = useMutation(signup)
+	const [signupMutation] = useMutation(signup);
 
-  return (
-    <div>
-      <h1>Create an Account</h1>
+	return (
+		<div className="flex flex-col justify-center items-center h-screen">
+			<div>
+				<h1>Create an Account</h1>
+				<Form
+					submitText="Create Account"
+					schema={Signup}
+					initialValues={{ email: '', password: '' }}
+					onSubmit={async (values) => {
+						try {
+							await signupMutation(values);
+							props.onSuccess?.();
+						} catch (error: any) {
+							if (error.code === 'P2002' && error.meta?.target?.includes('email')) {
+								// This error comes from Prisma
+								return { email: 'This email is already being used' };
+							} else {
+								return { [FORM_ERROR]: error.toString() };
+							}
+						}
+					}}
+				>
+					<LabeledTextField name="name" label="Name" placeholder="Name" />
+					<LabeledTextField name="email" label="Email" placeholder="Email" />
+					<LabeledTextField name="phone" label="" placeholder="phone" labelLeft="+91" />
+					<LabeledPasswordField
+						name="password"
+						label="Password"
+						placeholder="Password"
+						type="password"
+					/>
+					<LabeledPasswordField
+						name="passwordConfirmation"
+						label="Confirm Password"
+						placeholder="Conform Password"
+						type="password"
+					/>
+				</Form>
+			</div>
+		</div>
+	);
+};
 
-      <Form
-        submitText="Create Account"
-        schema={Signup}
-        initialValues={{ email: "", password: "" }}
-        onSubmit={async (values) => {
-          try {
-            await signupMutation(values)
-            props.onSuccess?.()
-          } catch (error: any) {
-            if (error.code === "P2002" && error.meta?.target?.includes("email")) {
-              // This error comes from Prisma
-              return { email: "This email is already being used" }
-            } else {
-              return { [FORM_ERROR]: error.toString() }
-            }
-          }
-        }}
-      >
-        <LabeledTextField name="email" label="Email" placeholder="Email" />
-        <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
-      </Form>
-    </div>
-  )
-}
-
-export default SignupForm
+export default SignupForm;
--- daily-bread-recipient/app/auth/validations.ts
+++ daily-bread-recipient/app/auth/validations.ts
@@ -1,42 +1,83 @@
-import { z } from "zod"
+import { z } from 'zod';
+
+enum userType {
+	INDIVIDUAL = 'INDIVIDUAL',
+	ORGANIZATION = 'ORGANIZATION',
+}
 
 export const email = z
-  .string()
-  .email()
-  .transform((str) => str.toLowerCase().trim())
+	.string()
+	.email()
+	.transform((str) => str.toLowerCase().trim());
+
+export const name = z
+	.string()
+	.min(3)
+	.transform((str) => str.toLowerCase().trim());
+export const city = z.string().optional();
+export const organizationId = z.string().optional();
+export const organizationName = z.string().optional();
+export const address = z.string().optional();
+export const state = z.string().optional();
+export const country = z.string().optional();
+export const totalMembers = z.number().min(1).optional();
+export const pinCode = z.string().min(6).max(6).optional();
+export const type = z.nativeEnum(userType).optional();
+export const phone = z
+	.string()
+	.max(10)
+	.min(10)
+	.transform((str) => str.toLowerCase().trim());
 
 export const password = z
-  .string()
-  .min(10)
-  .max(100)
-  .transform((str) => str.trim())
+	.string()
+	.min(10)
+	.max(100)
+	.transform((str) => str.trim());
 
-export const Signup = z.object({
-  email,
-  password,
-})
+export const Signup = z
+	.object({
+		name,
+		type,
+		phone,
+		email,
+		city,
+		state,
+		country,
+		pinCode,
+		address,
+		password,
+		passwordConfirmation: password,
+		totalMembers,
+		organizationId,
+		organizationName,
+	})
+	.refine((data) => data.password === data.passwordConfirmation, {
+		message: "Passwords don't match",
+		path: ['passwordConfirmation'], // set the path of the error
+	});
 
 export const Login = z.object({
-  email,
-  password: z.string(),
-})
+	email,
+	password: z.string(),
+});
 
 export const ForgotPassword = z.object({
-  email,
-})
+	email,
+});
 
 export const ResetPassword = z
-  .object({
-    password: password,
-    passwordConfirmation: password,
-    token: z.string(),
-  })
-  .refine((data) => data.password === data.passwordConfirmation, {
-    message: "Passwords don't match",
-    path: ["passwordConfirmation"], // set the path of the error
-  })
+	.object({
+		password: password,
+		passwordConfirmation: password,
+		token: z.string(),
+	})
+	.refine((data) => data.password === data.passwordConfirmation, {
+		message: "Passwords don't match",
+		path: ['passwordConfirmation'], // set the path of the error
+	});
 
 export const ChangePassword = z.object({
-  currentPassword: z.string(),
-  newPassword: password,
-})
+	currentPassword: z.string(),
+	newPassword: password,
+});
